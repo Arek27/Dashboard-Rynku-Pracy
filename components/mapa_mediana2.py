13c678e07ccb407e4a3b4c7419ac13b5987485f8
Arek27 <arek.galczynski@wp.pl>
2026-01-22 20:05:27 +0100

    Update mapa_mediana2.py

--- components/mapa_mediana2.py
+++ components/mapa_mediana2.py
@@ -11,7 +11,12 @@ def mediana_gminy(gdf, value_column: str):
         pickable=True,
         stroked=True,
         filled=True,
-        get_fill_color="properties.fill_color",
+        get_fill_color="""
+        selected ?
+        [255, 0, 0, 220] :
+        properties.fill_color
+    """
+
         get_line_color=[120, 120, 120],
         line_width_min_pixels=0.4,
     )
